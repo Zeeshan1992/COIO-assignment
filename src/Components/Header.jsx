469664768b1e9d1195e4f2d78a5b22814e3aaa38
zeeshan <zeeshan_1992@live.com>
2022-09-28 20:41:03 +0200

    hmm

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,45 +3,45 @@ import Button from './Button'
 
 export default function Header() {
     return (
-        <Div>
+        <Wrapper>
             <Logo src={'/logo_dark_small.png'} />
-            <SearchDiv>
+            <Search>
                 <button>
                     <Icon xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                     </Icon>
                 </button>
                 <SearchBar placeholder='Search' />
-            </SearchDiv>
+            </Search>
 
             <Button className="ml-auto" label={'COIO Time'} buttonType='primary'/>
-        </Div>
+        </Wrapper>
 
     )
 }
 
 
-const Div = styled.div`
+const Wrapper = styled.div`
     display: flex;
-    height: 3.5vw;
+    height: 80px;
     align-items: center;
-    padding-left: 10vw;
-    padding-right: 10vw;
+    padding-left: 185px;
+    padding-right: 165px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.05);
     background-color: #FFFFFF;
 `
 const Logo = styled.img`
-    height: 2.5vw;
-    width: 4vwx;
+    height: 55px;
+    width: 175px;
 `
-const SearchDiv = styled.div`
+const Search = styled.div`
     display: flex;
     align-items: center;
-    height: 1.5vw;
-    width: 35vw;
+    height: 35px;
+    width: 720px;
     border: 1px solid #B0B0B0;
     border-radius: 5px;
-    margin-left: 5vw;
+    margin-left: 90px;
 
 `
 const SearchBar = styled.input`
@@ -56,4 +56,3 @@ const Icon = styled.svg`
     width: 18px;
     margin-left: 12px;
 `
-
